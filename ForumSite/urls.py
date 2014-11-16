55cc4ce70ec5da104ae3078dc0fed023153b3de8
Alessio Linares <alessio@alessio.rocks>
2014-11-17 00:23:12 +0100

    Test project allow multiple forums

--- ForumSite/urls.py
+++ ForumSite/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^', include('Forum.urls'), {'forum_id':0}),
+    url(r'^(?P<forum_id>\d+)', include('Forum.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
